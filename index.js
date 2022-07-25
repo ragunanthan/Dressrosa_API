f5a914074d58091e93f98f5b2cbb3fb2ec014415
Ragunanthan <66114285+ragunanthan@users.noreply.github.com>
2022-07-25 14:53:54 +0530

    Added port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT | 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-}) 
+})   
