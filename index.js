ba3e0c6f5233309aa55157a8a8556c72a9f413b9
Ragunanthan <66114285+ragunanthan@users.noreply.github.com>
2022-07-25 19:44:53 +0530

    added config

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000;
+const express = require('express');
+const log = require('./logger');
+const app = express();
 
-app.get('/', (req, res) => {
+// middleware functions
+app.use(express.json());
+app.use(express.urlencoded({ extended : true })); //?key=value&key=value
+app.use(express.static('pulic')); //to store local assests
+app.use(log);
+
+console.log(`Node_env ${process.env.NODE_ENV}`);
+
+app.get('/api', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})   
+
+
+ 
+
+// port that listening 
+const port = process.env.PORT || 3000; 
+app.listen(port, () =>   console.log(`Example app listening on port ${port}`))          
