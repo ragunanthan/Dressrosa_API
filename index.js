2177eec0298387dd94a3f8efea4fdc6cb27bfaab
Ragunanthan Thangavel <ragunanthan.thangavel@eleviant.com>
2022-07-26 10:54:21 +0530

    Added configuration packages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 var morgan = require('morgan');
+const config = require('config');
 const log = require('./logger');
 const app = express();
 
@@ -9,19 +10,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }));  //urlencoded encoded values like '?key=value&key=value'
 app.use(express.static('pulic')); //to store local assests
 app.use(helmet()); //helmet helps to add addition header in http request for security
-app.use(log); //custom logger function to logging data
 
 // use only in development 
 if(app.get('env') === 'development'){
   app.use(morgan('tiny')); //HTTP request logger middleware 
+  app.use(log); //custom logger function to logging data
 }
 
+// configuration
 
 
 
 
 app.get('/api', (req, res) => {
-  res.send(`Node_env ${process.env.NODE_ENV} ${app.get('env')}`)
+  res.send('application mail ' + config.get('mail.host'))
 })
 
 
