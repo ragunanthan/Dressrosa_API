6659b1014b0a4e5d9d94e4d176b01d42c51e0153
Ragunanthan Thangavel <ragunanthan.thangavel@eleviant.com>
2022-07-26 10:10:00 +0530

    Added helmet and morgen middleware

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const helmet = require('helmet');
+var morgan = require('morgan');
 const log = require('./logger');
 const app = express();
 
 // middleware functions
 app.use(express.json());
-app.use(express.urlencoded({ extended : true })); //?key=value&key=value
+app.use(express.urlencoded({ extended : true }));  //urlencoded encoded values like '?key=value&key=value'
 app.use(express.static('pulic')); //to store local assests
-app.use(log);
+app.use(helmet()); //helmet helps to add addition header in http request for security
+app.use(log); //custom logger function to logging data
 
-console.log(`Node_env ${process.env.NODE_ENV}`);
+// use only in development 
+if(app.get('env') === 'development'){
+  app.use(morgan('tiny')); //HTTP request logger middleware 
+}
+
+
+
+
 
 app.get('/api', (req, res) => {
   res.send(`Node_env ${process.env.NODE_ENV} ${app.get('env')}`)
